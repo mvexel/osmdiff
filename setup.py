1ecf16942804db86b0c43bdb144b49ba53cd9f4b
Martijn van Exel <m@rtijn.org>
2017-11-19 11:32:55 -0700

    pypi setup things, version 0.1.2

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from distutils.core import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
     name='osmdiff',
-    version='0.1',
+    url='https://github.com/osmlab/osmdiff',
+    version='0.1.2',
     packages=['osmdiff'],
+    description="Interact with OpenStreetMap (Augmented) Diff files.",
     license='Apache 2.0',
-    long_description=open('README.md').read(),
+    author="Martijn van Exel",
+    author_email="m@rtijn.org",
+    download_url="",
+    keywords=["osm", "openstreetmap", "diff", "augmented diff"],
+    classifiers=[]
 )
