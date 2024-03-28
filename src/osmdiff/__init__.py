5950fe165a67e30c1aa1ae2adc8073d584ab4258
Martijn van Exel <m@rtijn.org>
2024-03-28 12:56:57 -0600

    get version from module

--- src/osmdiff/__init__.py
+++ src/osmdiff/__init__.py
@@ -1,3 +1,12 @@
+"""
+osmdiff is a Python library for working with OpenStreetMap changesets and diffs.
+
+It provides classes for working with OpenStreetMap changesets and diffs, and
+includes a parser for the OpenStreetMap changeset API.
+"""
+
+__version__ = [0, 4, 0]
+
 from .augmenteddiff import AugmentedDiff
 from .osm import Node, Relation, Way
 from .osmchange import OSMChange
